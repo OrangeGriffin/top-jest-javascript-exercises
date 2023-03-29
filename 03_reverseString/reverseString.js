fe045f4d798d1237e49d5afd214663a65ef4d658
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-03-29 10:17:52 -0500

    Pass test 1

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,24 @@
-const reverseString = function() {
+const reverseString = function (providedString) {
+  let reversedString = ""; // Create empty string for result placholder
+  const reversedStringArray = []; // Create empty array for the reverse array of the provided string
+  const myStrArray = providedString.split(""); // Create array from the providedString
+
+
+  // Loop through each letter in provided string
+  // Place that letter into the reversedStringArray
+  while (myStrArray.length > 0) {
+    let letter = myStrArray.pop();
+    reversedStringArray.push(letter);
+  }
+
+  // Loop through each letter in the reversedStringArray
+  // Add the letter at each position in the loop to reversedString
+  for (i=0; i < reversedStringArray.length; i++) {
+    let letter = reversedStringArray[i];
+    reversedString = reversedString + letter;
+  }
+
+  return reversedString;
 
 };
 
