a664b8ce634e9d77f7527ad6348b6ceb58cca8dc
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-03-29 11:33:48 -0500

    Add comment to main loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
 const removeFromArray = function(array, removeArg) {
+    
+    // Loop through array. Find if a value at an index is equal to the given argument.
+    // If match, remove this item from the array.
     for (i = 0; i < array.length; i++) {
         if (array[i] === removeArg) {
             array.splice(i, 1);
