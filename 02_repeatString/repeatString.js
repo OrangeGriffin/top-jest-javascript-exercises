7477657c7915a2743a48caefa1100685f9f83732
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-03-29 08:44:48 -0500

    Add error message for negative arguments

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (enteredString, numTimes) {
+  let resultString = "";
 
+  if (numTimes < 0) {
+    return "ERROR";
+  } else {
+    for (i = 0; i < numTimes; i++) {
+      resultString = resultString + enteredString;
+    }
+    return resultString;
+  }
 };
 
 // Do not edit below this line
