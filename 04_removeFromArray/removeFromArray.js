dfb7588a7879d28b14a83e488d3896006d8da807
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-03-29 10:46:53 -0500

    Pass test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, removeArg) {
+    for (i = 0; i < array.length; i++) {
+        if (array[i] === removeArg) {
+            array.splice(i, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
