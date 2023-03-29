3b467667f7c7476aa36cc1eed75403950d1b5e5e
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-03-29 16:06:19 -0500

    Passing 3 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let largeNum;
+  let smallNum;
+  let sum = 0;
 
+  if (num1 >= 0 && num2 >= 0) {
+    if (num1 > num2) {
+      largeNum = num1;
+      smallNum = num2;
+    } else {
+      largeNum = num2;
+      smallNum = num1;
+    }
+
+    for (i = smallNum; i <= largeNum; i++) {
+      sum = sum + i;
+    }
+    return sum;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
