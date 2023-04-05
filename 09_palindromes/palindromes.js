f4842d13db7fe69f55c24b14f75910e3fa902a9c
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-04-05 10:07:21 -0500

    Passing all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const lowerCase = str.toLowerCase();
+  const removedPunctuation = lowerCase.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, ""); // remove punctation
+  const charsOnly = removedPunctuation.replace(/\s/g, ""); // remove whitespace
+  const forward = charsOnly.split(""); // Place characters in array
+  const forwardCopy = [...forward];
+  const reversed = forwardCopy.reverse();
 
+let match = false;
+
+  for (i = 0; i < forward.length; i++) {
+    if (forward[i] === reversed[i]) {
+      match = true;
+    } else {
+      match = false;
+      return match;
+    }
+  }
+  return match;
 };
 
 // Do not edit below this line
