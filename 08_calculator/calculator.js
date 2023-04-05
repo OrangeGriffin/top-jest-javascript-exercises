35490b35251db292125872a2e0a8b365ac27de0a
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-04-05 06:37:05 -0500

    All tests passing

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,53 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  if (array.length === 0) {
+    return 0;
+  } else if (array.length === 1) {
+    return array[0];
+  } else {
+    return array.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+  }
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce(
+    (accumulator, currentValue) => accumulator * currentValue,
+    1
+  );
 };
 
-const power = function() {
-	
+const power = function (num, pow) {
+  return num**pow
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num > 2) {
+    let array = []
+    while (num >= 1) {
+      array.push(num);
+      num--;
+    }
+    return array.reduce(
+      (accumulator, currentValue) => accumulator * currentValue,
+      1
+    );
+  } else if (num === 2) {
+    return 2;
+  } else if (num === 1) {
+    return 1;
+  } else if (num === 0) {
+    return 1;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +57,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
