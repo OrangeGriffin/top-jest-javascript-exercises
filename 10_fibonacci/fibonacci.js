e9fbd7c9348325579d916a20caa453d8ca430e6a
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-04-05 12:49:05 -0500

    All tests passing

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(pos) {
+    pos = Number(pos);
+    const fibArray = [1,1];
+    let sum = 0;
 
+    if (pos < 0) {
+        return 'OOPS'
+    } else if (pos === 1 || pos === 2) {
+        return 1
+    }
+
+    for (i = 3; i <= pos; i++) {
+        sum = fibArray[fibArray.length-1] + fibArray[fibArray.length-2]
+        fibArray.push(sum);
+    }
+    return sum;
 };
 
 // Do not edit below this line
